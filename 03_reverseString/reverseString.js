8faa31d7952c1c1716a41abecd37878a8188e335
Barister <59793749+Barister@users.noreply.github.com>
2023-02-13 21:58:05 +0300

    03ex solved with arrays and for loop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let arrInput = input.split("");
+    let reversedArr = [];
+    for (let i = arrInput.length; i >= 0; i--) {
+        reversedArr.push(arrInput[i]);
+    }
+    return reversedArr.join("");
 };
 
 // Do not edit below this line
