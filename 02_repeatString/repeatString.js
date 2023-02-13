c60ac0aebc8777f02b6bcd63f1360cf81c0a022f
Barister <59793749+Barister@users.noreply.github.com>
2023-02-13 21:43:43 +0300

    repeatString with N times tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(text, times) {
+    let result = "";
 
+    if (times < 0) {
+        return "ERROR";
+    }
+
+    for (let i = 0; i < times; i++) {
+        result += text;
+    }
+    
+    return result;
 };
 
 // Do not edit below this line
