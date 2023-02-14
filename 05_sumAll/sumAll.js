d78674b205e5264c2cc5cf1c82fcefcc27622e52
Barister <59793749+Barister@users.noreply.github.com>
2023-02-14 21:23:33 +0300

    sumUp with for loop and some tests for numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(firstInt, lastInt) {
+    let sumUp = 0;
 
+    if (typeof firstInt != "number" || typeof lastInt != "number" || firstInt < 0 || lastInt < 0 ) {
+        return "ERROR";
+    }
+
+    else if (firstInt > lastInt) {
+        let temp = firstInt;
+        firstInt = lastInt;
+        lastInt = temp;
+    }
+
+    for (let i = firstInt; i <= lastInt; i++) {
+        
+        sumUp += i;
+    }
+    console.log("sumUp:", sumUp);
+    return sumUp;
 };
 
 // Do not edit below this line
