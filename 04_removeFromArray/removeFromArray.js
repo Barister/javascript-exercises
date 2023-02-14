cffb49b683186b22d6274ecbca50e54b039fed83
Barister <59793749+Barister@users.noreply.github.com>
2023-02-14 19:48:09 +0300

    04 solved with nested loops

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...element) {
+    let returnArray = [];
+    
+    label:
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < element.length; j++) {
+            if (array[i] !== element[j]) {
+                continue;
+            }  
+            else {continue label}
+        }
+        returnArray.push(array[i]);
+    }
+    console.log("Hello World!");
+    return returnArray;
 };
 
 // Do not edit below this line
