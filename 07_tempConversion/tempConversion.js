fab37258db24c597d06622184ede22f0e48fbeca
Barister <59793749+Barister@users.noreply.github.com>
2023-02-16 18:34:33 +0300

    Just tempConvsersion between Celsius and Fahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputTemp) {
+  let celsiusResult = ((inputTemp - 32) * 5 / 9);
+  let roundedCelsiusResult = Math.round(celsiusResult *  10) / 10;
+  console.log(roundedCelsiusResult);
+  return roundedCelsiusResult;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputTemp) {
+  let fahrenheitResult = Math.round((inputTemp * 9 /5 + 32) * 10) / 10;
+
+  console.log(fahrenheitResult);
+  return fahrenheitResult;
 };
 
 // Do not edit below this line
